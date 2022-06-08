f5dee6ad1eaf61a3aa6a70961bc1e7e64778dee9
Josef Fryk <josef.fryk@gmail.com>
2022-06-08 17:45:33 +0200

    fix

--- server.js
+++ server.js
@@ -47,7 +47,7 @@ app.use('/api/v1/jobs',authenticateUser, jobsRouter)
 app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
 
-const port = process.env.PORT || 4000;
+const port = process.env.PORT || 3000;
 
 const start = async () => {
   try {
